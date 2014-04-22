7c0967fab15366df39686b225ae47b2778143278
David Hernandez <dahernan@gmail.com>
2014-04-22 22:14:31 +0100

    basic implementation of statsd calls

--- export.go
+++ export.go
@@ -1,17 +1,23 @@
 package goHystrix
 
 import (
-	//"fmt"
+	"fmt"
 	"github.com/dahernan/goHystrix/statsd"
 	"time"
 )
 
 type StatsdExport struct {
-	statsdClient statsd.StatsdClient
+	statsdClient *statsd.StatsdClient
 	prefix       string
 }
 
 type NilExport struct {
+}
+
+func UseStatsd(host string, port int, prefix string) {
+	statsdClient := statsd.New(host, port)
+	export := NewStatsdExport(prefix, statsdClient)
+	SetExporter(export)
 }
 
 func NewNilExport() MetricExport { return NilExport{} }
@@ -23,13 +29,20 @@ func (NilExport) FallbackError(group string, name string)                   {}
 func (NilExport) Timeout(group string, name string)                         {}
 func (NilExport) Panic(group string, name string)                           {}
 
-func NewStatsdExport(prefix string, statsdClient statsd.StatsdClient) MetricExport {
+func NewStatsdExport(prefix string, statsdClient *statsd.StatsdClient) MetricExport {
 	return StatsdExport{statsdClient, prefix}
 }
 
-func (StatsdExport) Success(group string, name string, duration time.Duration) {}
-func (StatsdExport) Fail(group string, name string)                            {}
-func (StatsdExport) Fallback(group string, name string)                        {}
-func (StatsdExport) FallbackError(group string, name string)                   {}
-func (StatsdExport) Timeout(group string, name string)                         {}
-func (StatsdExport) Panic(group string, name string)                           {}
+func (s StatsdExport) Success(group string, name string, duration time.Duration) {
+	go func() {
+		s.statsdClient.Increment(fmt.Sprintf("%s.%s.%s.success", s.prefix, group, name))
+		ms := int64(duration / time.Millisecond)
+		s.statsdClient.Timing(fmt.Sprintf("%s.%s.%s.duration", s.prefix, group, name), ms)
+	}()
+}
+
+func (StatsdExport) Fail(group string, name string)          {}
+func (StatsdExport) Fallback(group string, name string)      {}
+func (StatsdExport) FallbackError(group string, name string) {}
+func (StatsdExport) Timeout(group string, name string)       {}
+func (StatsdExport) Panic(group string, name string)         {}
